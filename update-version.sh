4a3f852db709dcfb19633fa2d5dd0b9367189ad7
gobeumsu <gobeumsu@gmail.com>
2024-10-10 21:27:00 +0100

    Update blog

--- update-version.sh
+++ update-version.sh
@@ -1,6 +1,6 @@
 cd /Users/beomsu/Documents/quartz
 rsync -av --delete ~/Documents/Ataraxia/80.\ Blog/ ~/Documents/quartz/content/
-
+npx quartz update
 git add *
 git commit -m "Update blog"
 git push
